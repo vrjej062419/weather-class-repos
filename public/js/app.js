d0cbf991d36a8e507c661f10df53c0da333327e4
Victoria Jeffries <vrjej0624@gmail.com>
2021-05-16 23:36:39 -0500

    Setup for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit', (e) =>  {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address='+ inputData.value).then((response) => {
+    fetch('/weather?address='+ inputData.value).then((response) => {
         response.json().then((data) => {
             if(data.error){
                 messageOne.textContent = data.error
